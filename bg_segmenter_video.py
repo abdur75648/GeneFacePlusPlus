90cfcda4fce264ff2b441d158c380ae5bb58a4ac
Abdur <abdur75648@gmail.com>
2024-10-07 06:47:03 +0000

    Paste back corrected

--- bg_segmenter_video.py
+++ bg_segmenter_video.py
@@ -46,38 +46,49 @@ def process_frame(frame_info):
     green_screen_img = seg_model.apply_green_screen(img, segmap)
     return frame_num, cv2.cvtColor(green_screen_img, cv2.COLOR_RGB2BGR)
 
-def process_video(video_path, output_video_path, seg_model):
+def process_video(video_path, output_video_path, seg_model, chunk_size=1000):
     cap = cv2.VideoCapture(video_path)
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    assert fps == 25, "FPS of the video is not 25"
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for .mp4 files
     out_video = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
 
-    cpu_count = 2 # os.cpu_count()
+    if os.cpu_count() > 2:
+        cpu_count = os.cpu_count() - 2
+    else:
+        cpu_count = 1
     print(f"Using {cpu_count} cores for parallel processing")
 
-    frame_info_list = []
+    for start_frame in range(0, frame_count, chunk_size):
+        print(f"Processing frames {start_frame} to {min(start_frame + chunk_size, frame_count)}")
+        end_frame = min(start_frame + chunk_size, frame_count)
+        frame_info_list = []
 
-    for frame_num in range(frame_count):
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame_info_list.append((frame, frame_num, seg_model))
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        for frame_num in tqdm(range(start_frame, end_frame), desc="Reading frames"):
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame_info_list.append((frame, frame_num, seg_model))
+
+        with Pool(cpu_count) as p:
+            for frame_num, processed_frame in tqdm(p.imap(process_frame, frame_info_list), total=len(frame_info_list)):
+                out_video.write(processed_frame)
+        
+        del frame_info_list
 
     cap.release()
-
-    with Pool(cpu_count) as p:
-        for frame_num, processed_frame in tqdm(p.imap(process_frame, frame_info_list), total=frame_count):
-            out_video.write(processed_frame)
-
     out_video.release()
+    print("Segmentation Done - Saved!")
 
 if __name__ == '__main__':
-    video_path = "Don_Georgevich_3_692_20s_25fps.mp4"
-    output_video_path = "BG_Segmented_Don_Georgevich_3_692_20s.mp4"
+    video_path = "VID20240927102042.mp4"
+    output_video_path = "BG_Segmented_VID20240927102042.mp4"
     seg_model = MediapipeSegmenter()
 
     process_video(video_path, output_video_path, seg_model)
